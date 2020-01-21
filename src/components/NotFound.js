29e2d315b2cbe9a905fc61c3c6e09e5b37a7464e
Parieiko Vlad <parieiko.vlad@gmail.com>
2020-01-22 00:35:37 +0300

    Component rendering with a non-existent request

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+    return (
+        <>
+        <span className="not-found">Sorry, but your page hasn't been found :(</span>
+        </>
+    )
+}
