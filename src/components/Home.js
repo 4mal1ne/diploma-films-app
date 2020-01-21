f4f43059a8c877c1b9e997303497d8fd6f35e504
Parieiko Vlad <parieiko.vlad@gmail.com>
2020-01-22 00:17:06 +0300

    Navigation bar. Logo rendering

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <nav className="navigation">
+            <div className="nav-wrapper">
+                <a href="/home" className="brand-logo">Netflix cosplay</a>
+            </div>
+        </nav>
+    )
+};
+
+export default Home;
