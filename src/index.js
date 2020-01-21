09d292b17f6cc273c14242f0e53c10a1f5f1d1e6
Parieiko Vlad <parieiko.vlad@gmail.com>
2020-01-21 23:59:31 +0300

    The main '.js' file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+import './style/body.css';
+import './style/style.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
