0092454385c35a8a402a56de7eb2e21d47a9578f
Parieiko Vlad <parieiko.vlad@gmail.com>
2020-01-23 15:20:45 +0300

    removed the alt attribute in the img tag. Carried the back link to the end of the block.

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -3,22 +3,15 @@ import React from "react";
 const MovieInfo = (props) => {
     return (
         <div className="container">
-                <span className="go-back-link" onClick={props.closeMovieInfo}>Go back</span>
-
             <div className="movie-info-block">
                 <div className="image-block-info">
-                {
-                    props.currentMovie.poster_path === null ?
-                        <img src={"../image/not-found.png"}
-                            alt="card-film"/> :
-                        <img src={`https://image.tmdb.org/t/p/w185${props.currentMovie.poster_path}`}
-                            alt="card-film"/>
-                }
+                    <img src={`https://image.tmdb.org/t/p/w185${props.currentMovie.poster_path}`}/>
                 </div>
                 <div className="info-container">
                     <h1>{props.currentMovie.title}</h1>
                     <h3>{props.currentMovie.release_date}</h3>
                     <p>{props.currentMovie.overview}</p>
+                    <span className="go-back-link" onClick={props.closeMovieInfo}>Go back</span>
                 </div>
             </div>
         </div>
