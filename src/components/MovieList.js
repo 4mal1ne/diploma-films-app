08936ca4b2c4436612b37e3bcffc2664dcaab5d3
Parieiko Vlad <parieiko.vlad@gmail.com>
2020-01-23 15:24:23 +0300

    The component of displaying all query results. Rendering of the movie component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,10 +2,10 @@ import React from "react";
 import Movie from "./Movie";
 
 const MovieList  = (props) => {
-    return (
+    return ( 
         <div className="films-contain">
              {
-                 props.movies.map((movie, i) => {
+                props.movies.map((movie, i) => {
                      return (
                          <Movie key={i} viewMovieInfo={props.viewMovieInfo} movieId={movie.id} image={movie.poster_path} />
                      );
